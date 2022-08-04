958547e99795c50081543473dbb71a672da61be1
kotic03062543 <98500757+kotic03062543@users.noreply.github.com>
2022-08-04 23:48:36 +0700

    Design workshop
    Design workshop 040

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:myfirstapp/design_layout.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
